a703ff03feaccf32e984614cd7810426f3fe5518
Kunal <kunalnandwana1999@gmail.com>
2021-04-25 08:01:57 +0530

    taking Input and console

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("this script works")
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+
+console.log(txtInput);
+
+
+function clickHandler(){
+    console.log("clicked");
+    console.log("input",txtInput.value);
+}
+btnTranslate.addEventListener("click", clickHandler)
