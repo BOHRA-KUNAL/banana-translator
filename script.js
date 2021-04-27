3c10de0af1b79579660492a2b7db4699f9d91fb7
Kunal <kunalnandwana1999@gmail.com>
2021-04-27 19:18:18 +0530

    Completed banana translation

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 var btnTranslate = document.querySelector("#btn-translate");
-var txtInput=document.querySelector("#txt-input");
+var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-function clickHandler(){
-    outputDiv.innerText = "ajbdjasknd "+ txtInput.value;
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("Something went wrong with the server.Please try again later.")
+}
+
+function clickHandler() {
+    
+    var inputText = txtInput.value; //taking input
+
+    //calling server for processing
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
 };
 
 btnTranslate.addEventListener("click", clickHandler)
