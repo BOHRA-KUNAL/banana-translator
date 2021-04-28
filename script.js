5d68484e3f12e503c4510ca28a0a7cb5e383302a
Kunal <kunalnandwana1999@gmail.com>
2021-04-28 13:30:44 +0530

    Star war

--- script.js
+++ script.js
@@ -2,22 +2,19 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-
-var serverURL = "https://api.funtranslations.com/translate/minion.json"
+var serverURL = "https://api.funtranslations.com/translate/klingon.json"
 
 function getTranslationURL(text) {
     return serverURL + "?" + "text=" + text
 }
 
-
-function errorHandler(error){
+function errorHandler(error) {
     console.log("error occured", error);
     alert("Something went wrong with the server.Please try again later.")
 }
 
 function clickHandler() {
-    
+
     var inputText = txtInput.value; //taking input
 
     //calling server for processing
